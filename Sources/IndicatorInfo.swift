1cbf99725d4f06fcfee49a5edd242009ea23446b
Laurenz Lazarus <laurenz@lazarus-awk.at>
2018-09-14 08:04:31 +0200

    Init

--- Sources/IndicatorInfo.swift
+++ Sources/IndicatorInfo.swift
@@ -24,7 +24,7 @@
 
 import Foundation
 
-public struct IndicatorInfo {
+open class IndicatorInfo: ExpressibleByStringLiteral {
 
     public var title: String?
     public var image: UIImage?
@@ -32,49 +32,26 @@ public struct IndicatorInfo {
     public var accessibilityLabel: String?
     public var userInfo: Any?
     
-    public init(title: String?) {
-        self.title = title
-        self.accessibilityLabel = title
-    }
-    
-    public init(image: UIImage?, highlightedImage: UIImage? = nil, userInfo: Any? = nil) {
-        self.image = image
-        self.highlightedImage = highlightedImage
-        self.userInfo = userInfo
-    }
-    
-    public init(title: String?, image: UIImage?, highlightedImage: UIImage? = nil, userInfo: Any? = nil) {
-        self.title = title
-        self.accessibilityLabel = title
-        self.image = image
-        self.highlightedImage = highlightedImage
-        self.userInfo = userInfo
-    }
-    
-    public init(title: String?, accessibilityLabel:String?, image: UIImage?, highlightedImage: UIImage? = nil, userInfo: Any? = nil) {
-        self.title = title
-        self.accessibilityLabel = accessibilityLabel
-        self.image = image
-        self.highlightedImage = highlightedImage
-        self.userInfo = userInfo
+    public init(title: String, image: UIImage? = nil, highlightedImage: UIImage? = nil, userInfo: Any? = nil) {
+      self.title = title
+      self.accessibilityLabel = title
+      self.image = image
+      self.highlightedImage = highlightedImage
+      self.userInfo = userInfo
     }
 
-}
+  required public init(stringLiteral value: String) {
+    title = value
+    accessibilityLabel = value
+  }
 
-extension IndicatorInfo : ExpressibleByStringLiteral {
+  required public init(extendedGraphemeClusterLiteral value: String) {
+    title = value
+    accessibilityLabel = value
+  }
 
-    public init(stringLiteral value: String) {
-        title = value
-        accessibilityLabel = value
-    }
-
-    public init(extendedGraphemeClusterLiteral value: String) {
-        title = value
-        accessibilityLabel = value
-    }
-
-    public init(unicodeScalarLiteral value: String) {
-        title = value
-        accessibilityLabel = value
-    }
+  required public init(unicodeScalarLiteral value: String) {
+    title = value
+    accessibilityLabel = value
+  }
 }
